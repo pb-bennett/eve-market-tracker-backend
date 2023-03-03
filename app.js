9fd6ae8a780382e92b6d7610b5b6aa97be9d4db2
pb-bennett <pb.bennett.uk@gmail.com>
2023-03-03 15:59:35 +0100

    solving CORS issues

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+app.all('/', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
 
 // app.use(express.json());
 // app.use(express.static(`${__dirname}/public`));
